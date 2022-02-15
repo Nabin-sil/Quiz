a474c2811ca4f49e831637dc7e160031ff73361a
Nabin-sil <69471540+Nabin-sil@users.noreply.github.com>
2022-02-15 18:10:01 +0545

    Add files via upload

---
+++ Frontend/src/app/_services/index.ts
@@ -0,0 +1,10 @@
+﻿export * from './account.service';
+export * from './alert.service';
+export * from './notification.service';
+export * from './interaction.service';
+export * from './quiz.service';
+
+
+
+
+
